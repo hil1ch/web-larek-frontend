76fca0494f5c64ffe6ee3e91a24b3a0a5ab49827
Илья <ilya.panarin14@gmail.com>
2025-03-06 19:37:15 +0500

    add simple functionality for BasketModel

--- src/components/models/BasketModel.ts
+++ src/components/models/BasketModel.ts
@@ -3,3 +3,25 @@ interface IBasketModel {
    add(id: string): void;      // метод добавления товара по его идентификатору
    remove(id: string): void;   // метод удаления товара по его идентификатору
 }
+
+
+export class BasketModel implements IBasketModel {
+   items: Map<string, number> = new Map();
+
+   add(id: string): void {
+      if (this.items.has(id)) {
+         this.items.set(id, 0);
+      } else {
+         this.items.set(id, this.items.get(id)! + 1);
+      }
+   }
+
+   remove(id: string): void {
+      if (!this.items.has(id)) return; // если корзина пуста
+
+      if (this.items.get(id)! > 0) { // иначе
+         this.items.set(id, this.items.get(id)! - 1);
+         if (this.items.get(id) === 0) this.items.delete(id);
+      }
+   }
+}
