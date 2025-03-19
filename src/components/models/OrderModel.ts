a0e933ca1285af1fd52f77dafd992cfd5025d93b
Илья <ilya.panarin14@gmail.com>
2025-03-19 21:50:07 +0500

    add OrderModel

--- src/components/models/OrderModel.ts
+++ src/components/models/OrderModel.ts
@@ -1,7 +1,43 @@
 import { IOrderForm } from "../../types/types";
+import { EventEmitter } from "../base/events";
 
 interface IOrderModel  {
    orderForm: IOrderForm;
    setInput(orderForm: Partial<IOrderForm>): void;
    reset(): void;
+}
+
+export class OrderModal implements IOrderModel {
+   orderForm: IOrderForm;
+   protected _submit: HTMLButtonElement;
+   protected _events: EventEmitter | null = null;
+
+   constructor(events: EventEmitter) {
+      this._events = events;
+   }
+
+   // метод обновляет данные формы
+   setInput(orderForm: Partial<IOrderForm>): void {
+      this.orderForm = {
+         ...this.orderForm,
+         ...orderForm,
+      }
+   }
+ 
+   // метод сбрасывает форму
+   reset() {
+      this.orderForm = {
+         payment: '',
+         address: '',
+         phone: '',
+         email: '',
+         total: this.orderForm.total,
+         items: [...this.orderForm.items],
+      }
+   }
+
+   // управление состоянием кнопки
+   set disableSubmitButton(flag: boolean) {
+      this._submit.disabled = !flag;
+   }
 }
